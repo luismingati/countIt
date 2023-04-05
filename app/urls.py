93ec10c7d5d93c2d5a43264e871bb5b8064d3e03
Pedro Andriotti <phab@cesar.school>
2023-04-05 17:42:14 -0300

    Adding Homepage

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductCreate, ProductList, ProductDetail
+from .views import ProductCreate, ProductList, ProductDetail, Home
+from . import views
 
 
 urlpatterns=[
+    path('', Home.as_view(), name='home'),
     path('estoque/', ProductList.as_view(), name= 'products'),
     path('estoque/<int:pk>/', ProductDetail.as_view(), name='product'),
     path('estoque/cadastro/', ProductCreate.as_view(), name='product-create')
--- app/views.py
+++ app/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from .models import Product
+
+class Home(TemplateView):
+    template_name = 'app/index.html'
+    context_object_name = 'home'
 
 class ProductList(ListView):
     model = Product
@@ -17,5 +22,5 @@ class ProductCreate(CreateView):
     model = Product
     fields = '__all__'
     success_url = reverse_lazy('products')
-    context_object_name = 'products'
+
 
