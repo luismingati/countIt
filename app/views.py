f499e79c8881a232e74d02f9b36e485c2d0c22d2
netoguerradev <valterguerradev@hotmail.com>
2023-04-05 10:08:35 -0300

    add render

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def productList(request):
-    return HttpResponse('Luis Otavius')
+    return render(request, 'index.html')
+
