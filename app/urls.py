2e5a13de1eb0da8179931308f04d78e4d1c70120
luis <luismingati@gmail.com>
2023-04-05 16:55:32 -0300

    Create products ok

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import ProductCreate
 
 
 urlpatterns=[
     path('', views.productList, name= 'products'),
+    path('cadastro/', ProductCreate.as_view(), name='product-create'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Product
 
 def productList(request):
     return render(request, 'index.html')
 
+class ProductCreate(CreateView):
+    model = Product
+    fields = '__all__'
+    success_url = reverse_lazy('products')
