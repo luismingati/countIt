9dc6356fe3010d00783ba3b25c903c6acf14afd6
luis <luismingati@gmail.com>
2023-04-11 16:36:52 -0300

    validating quantity and price

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=150, null=True, blank=True, unique=True)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=9, decimal_places=2)
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    price = models.DecimalField(max_digits=9, decimal_places=2, validators=[MinValueValidator(0.1)])
 
     def __str__(self): 
         return self.name
