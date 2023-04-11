72e1ca1eceb1e300b522a6942d9480cb8dd8fa2e
Pedro Andriotti <phab@cesar.school>
2023-04-11 12:42:18 -0300

    Register Feature and Routing

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import ProductCreate, ProductList, ProductDetail, Home, Login
+from .views import ProductCreate, ProductList, ProductDetail, Home, Login, Register
 from django.contrib.auth.views import LogoutView
-
 
 urlpatterns=[
     path('', Home.as_view(), name='home'),
     path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
     path('estoque/', ProductList.as_view(), name= 'products'),
     path('estoque/<int:pk>/', ProductDetail.as_view(), name='product'),
     path('estoque/cadastro/', ProductCreate.as_view(), name='product-create'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout')
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
-from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, FormView
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from .models import Product
-from django.http import Http404
 
 class Home(TemplateView):
     template_name = 'app/index.html'
@@ -19,10 +21,32 @@ class Login(LoginView):
     
     def get_success_url(self):
         return reverse_lazy('products')
+    
+class Register(FormView):
+    template_name = 'app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('products')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Register, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('products')
+        return super(Register, self).get(*args, **kwargs)
 
 class ProductList(LoginRequiredMixin, ListView):
     model = Product
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = context['products'].filter(user = self.request.user)
+        return context
 
 class ProductDetail(LoginRequiredMixin, DetailView):
     model = Product
@@ -30,9 +54,16 @@ class ProductDetail(LoginRequiredMixin, DetailView):
 
 class ProductCreate(LoginRequiredMixin, CreateView):
     model = Product
-    fields = '__all__'
+    fields = [
+        'name',
+        'quantity',
+        'price',
+    ]
     success_url = reverse_lazy('products')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(ProductCreate, self).form_valid(form)
 
 
 
