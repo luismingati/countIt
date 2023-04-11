eb0cbb8268601ab1103f7e94ee69a721d68ed6d0
luis <luismingati@gmail.com>
2023-04-11 18:08:43 -0300

    nothing

--- app/urls.py
+++ app/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth.views import LogoutView
 urlpatterns=[
     path('', Home.as_view(), name='home'),
     path('login/', Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('register/', Register.as_view(), name='register'),
     path('estoque/', ProductList.as_view(), name= 'products'),
     path('estoque/<int:pk>/', ProductDetail.as_view(), name='product'),
     path('estoque/cadastro/', ProductCreate.as_view(), name='product-create'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
