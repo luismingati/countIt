18275953fa4164c05b28e77cd768bae5eee4c6e9
netoguerradev <valterguerradev@hotmail.com>
2023-04-12 12:26:58 -0300

    delete page front-end

--- project/settings.py
+++ project/settings.py
@@ -119,6 +119,10 @@ LOGIN_URL = 'login'
 
 STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ('static',)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
